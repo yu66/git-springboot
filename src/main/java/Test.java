849d974ece67e1f571bc168d41cbbf3f240f1c50
yu66 <3179046054@qq.com>
2022-04-11 16:39:50 +0800

    git springboot 2022 4 11 222

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("git springboot" +
                 "");
-        System.out.println("sssssssbbbbbbbbbbbbbb");
+        System.out.println("ssssss");
     }
 }
