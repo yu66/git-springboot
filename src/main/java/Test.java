4dfd74788fbdb9fd5208a7d26815b3817f12cd13
yu66 <52215154+yu66@users.noreply.github.com>
2022-04-11 16:44:39 +0800

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("git springboot" +
                 "");
-        System.out.println("ssssssbbbbbbbbbbbbbbbbbbccccccccccccc");
+        System.out.println("ssssssbbbbbs");
     }
 }
